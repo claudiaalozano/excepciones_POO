3fadcd80e0f9141a7a7cf90ddf9151a728a4ffaa
claudiaalozano <clau.lozano.perez@gmail.com>
2022-04-01 09:16:00 +0200

    creo la clase y el constructor

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
 import re
+
+class Correo:
+
+    def __init__ (self, error):
+        self.error = error
+    def set_busquedaerror (self,correo_electronico, error):
+        try:
+            
+
+
+
+
+
+correo_electronico = input("introduzca el correo electrónico para comenzar su registro: ")
