db2e1f52850bedd63ec15488f450e574ad26bf9d
claudiaalozano <clau.lozano.perez@gmail.com>
2022-04-03 17:43:28 +0200

    modifico errores

--- Clases/Ejercicio1.py
+++ Clases/Ejercicio1.py
@@ -6,14 +6,14 @@ class Correo:
         self.error = error
     def error (self, correo_electronico, error):
         try:
-            re.search(". * @. * \ .. *", correo_electronico)
+            re.search(" @ * . * .com *", correo_electronico)
             print("El correo electronico es correcto, bienvenido a la página web.")
         except:
             error_ortografia = re.search(". * @. * \ .. *", correo_electronico)
             if correo_electronico != error_ortografia:
                 print("Lo sentimos, introduzca una direccion de correo valida.")  
         else:
-            
+
         
 
 
