16f7ef3b498e11e3f12c5fe83a7078dbd36c5f1f
claudiaalozano <clau.lozano.perez@gmail.com>
2022-04-03 14:26:36 +0200

    commit

--- Clases/Ejercicio1.py
+++ Clases/Ejercicio1.py
@@ -1,11 +1,10 @@
-from cgi import print_directory
 import re
 
 class Correo:
 
     def __init__ (self, error):
         self.error = error
-    def set_busquedaerror (self,correo_electronico, error):
+    def error (self, correo_electronico, error):
         try:
             re.search(". * @. * \ .. *", correo_electronico)
             print("El correo electronico es correcto, bienvenido a la página web.")
@@ -13,18 +12,10 @@ class Correo:
             error_ortografia = re.search(". * @. * \ .. *", correo_electronico)
             if correo_electronico != error_ortografia:
                 print("Lo sentimos, introduzca una direccion de correo valida.")  
+        else:
+            
         
-    def get_busquedaerror (self):
-        print(set.busquedaerror)
 
 
             
-
-
-
-
-
-
-
-
 correo_electronico = input("introduzca el correo electrónico para el inicio de sesión: ")
