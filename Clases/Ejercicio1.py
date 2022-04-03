7bf74179b0c94f0664720f0bf894b2fbe7c0def0
claudiaalozano <clau.lozano.perez@gmail.com>
2022-04-03 17:45:04 +0200

    creo los correos registrados

--- Clases/Ejercicio1.py
+++ Clases/Ejercicio1.py
@@ -2,14 +2,15 @@ import re
 
 class Correo:
 
-    def __init__ (self, error):
+    def __init__ (self, error,correo):
         self.error = error
+        self.correo = ["vicente@gmail.com" , "claudia@gmail.com"]
     def error (self, correo_electronico, error):
         try:
             re.search(" @ * . * .com *", correo_electronico)
             print("El correo electronico es correcto, bienvenido a la página web.")
         except:
-            error_ortografia = re.search(". * @. * \ .. *", correo_electronico)
+            error_ortografia = re.search(" @ * . * .com *", correo_electronico)
             if correo_electronico != error_ortografia:
                 print("Lo sentimos, introduzca una direccion de correo valida.")  
         else:
